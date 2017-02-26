076888af07e6bcd9ed0363b808a49787f2b81c1b
frostblooded <frostblooded@yahoo.com>
2017-02-26 14:29:41 +0200

    Add test app

--- image_site_app/views.py
+++ image_site_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.utils import timezone
+
+def index(request):
+    return HttpResponse(timezone.localtime(timezone.now()))
