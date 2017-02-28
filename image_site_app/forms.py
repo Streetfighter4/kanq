2d5c5a1bf693f428b53f8d4a6e788f7be864aa9e
frostblooded <frostblooded@yahoo.com>
2017-02-28 10:17:20 +0200

    Add placeholder to first_name and last_name fields in signup form

--- image_site_app/forms.py
+++ image_site_app/forms.py
@@ -4,8 +4,19 @@ from django import forms
 class SignupForm(forms.Form):
     field_order = ['username', 'first_name', 'last_name', 'email', 'password', 'password2']
 
-    first_name = forms.CharField(max_length=30, label='First name (optional)', required=False)
-    last_name = forms.CharField(max_length=30, label='Last name (optional)', required=False)
+    first_name = forms.CharField(max_length=30,
+                                 label='First name (optional)',
+                                 required=False,
+                                 widget=forms.TextInput(attrs={
+                                     'placeholder': 'First name'
+                                 }))
+
+    last_name = forms.CharField(max_length=30,
+                                label='Last name (optional)',
+                                required=False,
+                                widget=forms.TextInput(attrs={
+                                    'placeholder': 'Last name'
+                                }))
 
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
