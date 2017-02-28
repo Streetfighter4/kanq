c144719fe208adf334ea82676ac6d91a526d1a93
frostblooded <frostblooded@yahoo.com>
2017-02-28 09:40:42 +0200

    Add bootstrap3 to project

--- image_site/settings.py
+++ image_site/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'image_site_app',
+
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
