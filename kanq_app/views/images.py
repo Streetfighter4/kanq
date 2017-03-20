df06d734b28e3f2cbf891b7d9cba2ad0b2e56e99
Antonio Mindov <amindov@abv.bg>
2017-03-21 00:20:13 +0200

    Full back end workflow example

---
+++ kanq_app/views/images.py
@@ -0,0 +1,23 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from kanq_app.models import Image
+from kanq_app.serializers import ImageSerializer
+
+
+class ImageList(APIView):
+
+    def get(self, request, format=None):
+        allImages = Image.objects.all()
+        serializer = ImageSerializer(allImages, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ImageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
