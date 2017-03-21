b59b7ce74c284960b676bdf3a1d42577f064f363
frostblooded <frostblooded@yahoo.com>
2017-03-21 07:51:11 +0200

    Remove old app

--- kanq_app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models.image import Image
-
-
-class ImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Image
-        fields = ('id', 'uri', 'createdAt')
