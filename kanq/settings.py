4a067308b51f04d26fc0cace9a8a7cf44df198ab
frostblooded <frostblooded@yahoo.com>
2017-03-30 00:20:17 +0300

    Add production IP to allowed hosts

--- kanq/settings.py
+++ kanq/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'kanq.wsgi.application'
+ALLOWED_HOSTS = ['37.157.138.76']
 
 
 # Database
@@ -123,7 +124,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
