35127ae3edcf15f8c87e47eaeac93b86f5114ace
frostblooded <frostblooded@yahoo.com>
2017-04-02 19:58:49 +0300

    Some general changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
-  template: `
-    <navbar></navbar>
-    <h1>Hello {{name}}</h1>
-    <form action="/oauth/facebook/authorize" method="GET">
-      <button type="submit" class="btn btn-primary">Facebook login</button>
-    </form>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: 'app.component.html',
 })
 export class AppComponent  {
   name = 'Kanq';
--- client/src/app/navbar/navbar.module.ts
+++ client/src/app/navbar/navbar.module.ts
@@ -1,7 +1,11 @@
 import {NgModule} from '@angular/core';
 import {NavbarComponent} from './navbar.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
+  imports: [
+    RouterModule
+  ],
   declarations: [
     NavbarComponent
   ],
