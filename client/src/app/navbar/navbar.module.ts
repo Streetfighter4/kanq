363add64ca96703fe4e6b1cc25d76e800cec1949
frostblooded <frostblooded@yahoo.com>
2017-04-02 10:45:14 +0300

    Some fixes

--- client/src/app/navbar/navbar.module.ts
+++ client/src/app/navbar/navbar.module.ts
@@ -1,11 +1,7 @@
 import {NgModule} from '@angular/core';
 import {NavbarComponent} from './navbar.component';
-import {RouterModule} from '@angular/router';
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot([{path: '', component: NavbarComponent}])
-  ],
   declarations: [
     NavbarComponent
   ],
