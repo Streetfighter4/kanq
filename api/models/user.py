f57fc708deceed65491e2cbe4cde7f16c936b5b0
Yasen Alexiev <yasen@lenovo37>
2017-04-04 11:31:13 +0300

    make users firld require

--- api/models/user.py
+++ api/models/user.py
@@ -4,10 +4,15 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-
+from django import forms
 
 class User(AbstractUser):
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(max_length=50, blank=False)
+
     following = models.ManyToManyField('self', related_name='followers', symmetrical=False)
+
 
     @receiver(post_save, sender=settings.AUTH_USER_MODEL)
     def create_auth_token(sender, instance=None, created=False, **kwargs):
