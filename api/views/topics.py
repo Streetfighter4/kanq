e48a15796377f5479d2ca0355b808394f905c42b
frostblooded <frostblooded@yahoo.com>
2017-04-07 21:36:27 +0300

    Remove debug code

--- api/views/topics.py
+++ api/views/topics.py
@@ -13,20 +13,12 @@ class TopicViewSet(viewsets.ModelViewSet):
     @list_route()
     def active(self, request):  # Get all active topics
         active_topics = [t for t in Topic.objects.all() if t.is_active()]
-
-        for topic in active_topics:
-            print(topic.is_active())
-
         serializer = TopicSerializer(instance=active_topics, many=True)
         return Response(serializer.data)
 
     @list_route()
     def inactive(self, request):  # Get all inactive topics
         inactive_topics = [t for t in Topic.objects.all() if not t.is_active()]
-
-        for topic in inactive_topics:
-            print(topic.is_active())
-
         serializer = TopicSerializer(instance=inactive_topics, many=True)
         return Response(serializer.data)
 
